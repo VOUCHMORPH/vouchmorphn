1c354a1cacf4e985adfe174c73944a54d4a3886b
marvinsehunelo <marvinsehunelo@gmail.com>
2026-05-24 15:56:51 +0200

    Create bank_formats.php

---
+++ src/Core/Config/Countries/South Africa/bank_formats.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    // South Africa bank message formats (no hardcoding in code)
+    'bank_formats' => [
+        'FNB' => 'legacy',
+        'ZURUBANK-SOUTHAFRICA' => 'iso20022',
+        'MTN' => 'mobile_money'
+    ],
+    
+    'default_format' => 'legacy'
+];
