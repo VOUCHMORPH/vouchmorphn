a55cea11910a7616202bdc2df153a8dd748050f7
marvinsehunelo <marvinsehunelo@gmail.com>
2026-05-25 12:42:05 +0200

    Update admin_login.php

--- public/admin/admin_login.php
+++ public/admin/admin_login.php
@@ -56,7 +56,7 @@ require_once PROJECT_ROOT . '/src/Application/Admin/Auth/AdminAuth.php';
 
 use Core\Database\DBConnection;
 use Application\Utils\SessionManager;
-use ADMIN_LAYER\Auth\AdminAuth;
+use Application\Admin\Auth\AdminAuth;
 
 // Initialize database connection from config
 try {
