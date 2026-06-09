daae367b6ba6b0cc1cdaaa8fb7ed864544ca3272
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-09 16:16:29 +0200

    Update tested.php

--- public/admin/tested.php
+++ public/admin/tested.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-
+require_once __DIR__ . '/../../tests/System/SwapServiceFullTestSuite.php';
 /**
  * Safe PostgreSQL connection bootstrap
  */
