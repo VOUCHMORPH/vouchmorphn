a8dfb35fc436d93d2ab2d05aa3096ac4fe21920e
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-11 00:19:59 +0200

    Update openfile.php

--- public/admin/openfile.php
+++ public/admin/openfile.php
@@ -1,4 +1,4 @@
 <?php
 // /var/www/html/public/call-swap.php
  
-require_once __DIR__ . '/../../../src/Domain/Services/SwapService.php';
+require_once __DIR__ . '/../../src/Domain/Services/SwapService.php';
