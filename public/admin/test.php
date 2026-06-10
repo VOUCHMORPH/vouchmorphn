30154d032a0646b12d597150fd2aa4a683babb68
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-10 10:02:26 +0200

    Update test.php

--- public/admin/test.php
+++ public/admin/test.php
@@ -13,7 +13,7 @@
  * Tests: Zurubank Voucher (200) → Saccussalis Cashout
  */
 
-require_once __DIR__ . '/../src/bootstrap.php';
+require_once __DIR__ . '/../../src/bootstrap.php';
 
 use Domain\Services\SwapService;
 use Domain\Services\FeeService;
