1826ccbc26dcfefdea54300903e3296501ccd5cd
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-10 12:40:42 +0200

    Update openfile.php

--- public/admin/openfile.php
+++ public/admin/openfile.php
@@ -1,4 +1,4 @@
 <?php
 // /var/www/html/public/call-swap.php
 
-require_once __DIR__ . '/../../src/Core/Database/DBConnection.php';
+require_once __DIR__ . '/../../src/Infrastructure/Banks/GenericBankClient.php';
