85adfc444b1580408114c21d8ef9770814bb6ebf
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-11 22:49:16 +0200

    Update tested.php

--- public/admin/tested.php
+++ public/admin/tested.php
@@ -1,7 +1,7 @@
 <?php
 // /public/sign_test.php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 use Infrastructure\Crypto\MessageSigner;
 
 header("Content-Type: text/plain");
