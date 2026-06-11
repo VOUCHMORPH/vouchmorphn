1522e4fc2d59c917259344069374ad2456090e37
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-11 21:25:49 +0200

    Update tested.php

--- public/admin/tested.php
+++ public/admin/tested.php
@@ -1,7 +1,7 @@
 <?php
 // /public/generate_signature.php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Infrastructure\Crypto\MessageSigner;
 
