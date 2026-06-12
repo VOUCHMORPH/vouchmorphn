2091f92c1f01e0493d90b1793a81dba5ddede071
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-12 16:49:50 +0200

    Update tested.php

--- public/admin/tested.php
+++ public/admin/tested.php
@@ -4,7 +4,7 @@
 
 // Include the classes
 require_once __DIR__ . '/../../src/Infrastructure/Crypto/CertificateManager.php';
-require_once __DIR__ . '/../../../src/Infrastructure/Crypto/MessagerSigner.php';
+require_once __DIR__ . '/../../src/Infrastructure/Crypto/MessagerSigner.php';
 
 use Infrastructure\Crypto\CertificateManager;
 use Infrastructure\Crypto\MessagerSigner;
