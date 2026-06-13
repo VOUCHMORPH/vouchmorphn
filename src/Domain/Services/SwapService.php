ebdbcb9296407e1bbc1dec60a40ad0707b754cec
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-13 17:21:40 +0200

    Update SwapService.php

--- src/Domain/Services/SwapService.php
+++ src/Domain/Services/SwapService.php
@@ -132,7 +132,7 @@ class SwapService
         
         // Initialize services
         $this->settlement = new HybridSettlementStrategy($this->swapDB);
-        $this->feeService = new FeeService($this->feesConfig, $this->config['currency'] ?? 'BWP');
+        $this->feeService = new FeeService($this->feesConfig, $this->config);
         $this->feeService->setParticipants($this->participants); 
         $this->forexService = new ForexService($this->swapDB, $this->config, $this->participants, $this->feeService);
         
