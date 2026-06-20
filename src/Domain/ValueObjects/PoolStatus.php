b52aa322d24b19dd4229437679c810fa401eebb4
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-20 11:23:12 +0200

    Create PoolStatus.php

---
+++ src/Domain/ValueObjects/PoolStatus.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Domain\ValueObjects;
+
+enum PoolStatus: string
+{
+    case CREATED = 'CREATED';
+    case VERIFYING = 'VERIFYING';
+    case HOLDING = 'HOLDING';
+    case FUNDED = 'FUNDED';
+    case DESTINATION_PENDING = 'DESTINATION_PENDING';
+    case DESTINATION_COMPLETED = 'DESTINATION_COMPLETED';
+    case DEBITING = 'DEBITING';
+    case SETTLING = 'SETTLING';
+    case INVOICING = 'INVOICING';
+    case COMPLETED = 'COMPLETED';
+    case FAILED = 'FAILED';
+    case CANCELLED = 'CANCELLED';
+    case ROLLED_BACK = 'ROLLED_BACK';
+}
