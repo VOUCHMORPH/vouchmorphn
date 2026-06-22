5fa61e9fb6e5c13f7398f84c67d567a21cc5a6a9
marvinsehunelo <marvinsehunelo@gmail.com>
2026-06-22 08:37:37 +0200

    Update SwapService.php

--- src/Domain/Services/SwapService.php
+++ src/Domain/Services/SwapService.php
@@ -21,8 +21,7 @@ use Infrastructure\Crypto\SignatureVerifier;
 use Infrastructure\Crypto\MessageSigner;
 use Infrastructure\Crypto\CertificateManager;
 use Infrastructure\Crypto\AggregateSigner;
-use Stringable;
-use Psr\Log\LoggerInterface;
+
 
 /**
  * SIGNED ATOMIC SWAP ORCHESTRATOR
@@ -96,35 +95,35 @@ class SwapService
         $this->countryCode = strtoupper($country);
         
         if ($logger === null) {
-    $this->logger = new class implements LoggerInterface {
-        public function emergency(Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][EMERGENCY] " . $message . " " . json_encode($context));
-        }
-        public function alert(Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][ALERT] " . $message . " " . json_encode($context));
-        }
-        public function critical(Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][CRITICAL] " . $message . " " . json_encode($context));
-        }
-        public function error(Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][ERROR] " . $message . " " . json_encode($context));
-        }
-        public function warning(Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][WARNING] " . $message . " " . json_encode($context));
-        }
-        public function notice(Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][NOTICE] " . $message . " " . json_encode($context));
-        }
-        public function info(Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][INFO] " . $message . " " . json_encode($context));
-        }
-        public function debug(Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][DEBUG] " . $message . " " . json_encode($context));
-        }
-        public function log($level, Stringable|string $message, array $context = []): void {
-            error_log("[SwapService][{$level}] " . $message . " " . json_encode($context));
-        }
-    };
+    $this->logger = new class {  // Remove "implements LoggerInterface"
+    public function emergency($message, array $context = []) {
+        error_log("[SwapService][EMERGENCY] " . $message . " " . json_encode($context));
+    }
+    public function alert($message, array $context = []) {
+        error_log("[SwapService][ALERT] " . $message . " " . json_encode($context));
+    }
+    public function critical($message, array $context = []) {
+        error_log("[SwapService][CRITICAL] " . $message . " " . json_encode($context));
+    }
+    public function error($message, array $context = []) {
+        error_log("[SwapService][ERROR] " . $message . " " . json_encode($context));
+    }
+    public function warning($message, array $context = []) {
+        error_log("[SwapService][WARNING] " . $message . " " . json_encode($context));
+    }
+    public function notice($message, array $context = []) {
+        error_log("[SwapService][NOTICE] " . $message . " " . json_encode($context));
+    }
+    public function info($message, array $context = []) {
+        error_log("[SwapService][INFO] " . $message . " " . json_encode($context));
+    }
+    public function debug($message, array $context = []) {
+        error_log("[SwapService][DEBUG] " . $message . " " . json_encode($context));
+    }
+    public function log($level, $message, array $context = []) {
+        error_log("[SwapService][{$level}] " . $message . " " . json_encode($context));
+    }
+};
 } else {
     $this->logger = $logger;
 }
