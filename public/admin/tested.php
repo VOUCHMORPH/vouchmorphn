b1c43e3f5990a17b53836345ab1b1674da85e782
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-15 10:54:56 +0200

    Update path for DBConnection in tested.php

--- public/admin/tested.php
+++ public/admin/tested.php
@@ -4,7 +4,7 @@
  * Run: php test_schema.php
  */
 
-require_once __DIR__ . '/src/Core/Database/DBConnection.php';
+require_once __DIR__ . '/../../src/Core/Database/DBConnection.php';
 
 use Core\Database\DBConnection;
 
