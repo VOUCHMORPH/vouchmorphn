9cb2ac1f0f39c0e8c52ac3b76fb609754f9318a2
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-19 19:02:42 +0200

    Update pending_claims.php

--- public/api/v1/swap/pending_claims.php
+++ public/api/v1/swap/pending_claims.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 header('Content-Type: application/json');
 
-require_once __DIR__ . '/../../../src/Application/Utils/SessionManager.php';
+require_once __DIR__ . '/../../../../src/Application/Utils/SessionManager.php';
 use Application\Utils\SessionManager;
 
 SessionManager::start();
@@ -24,10 +24,10 @@ if (empty($userId)) {
     exit;
 }
 
-require_once __DIR__ . '/../../../src/Core/Database/DBConnection.php';
-require_once __DIR__ . '/../../../vendor/autoload.php';
-require_once __DIR__ . '/../../../src/Domain/Services/SwapService.php';
-require_once __DIR__ . '/../../../src/Core/Config/LoadCountry.php';
+require_once __DIR__ . '/../../../../src/Core/Database/DBConnection.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../src/Domain/Services/SwapService.php';
+require_once __DIR__ . '/../../../../src/Core/Config/LoadCountry.php';
 
 use Core\Database\DBConnection;
 use Domain\Services\SwapService;
