29cebbd4c96665ba9aae1930aba5e5ad31db01e5
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-19 19:03:07 +0200

    Update set_pin.php

--- public/api/v1/swap/set_pin.php
+++ public/api/v1/swap/set_pin.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 header('Content-Type: application/json');
 
-require_once __DIR__ . '/../../../src/Application/Utils/SessionManager.php';
+require_once __DIR__ . '/../../../../src/Application/Utils/SessionManager.php';
 use Application\Utils\SessionManager;
 
 SessionManager::start();
@@ -34,10 +34,10 @@ if ($pin === '' || $pin !== $confirmPin) {
     exit;
 }
 
-require_once __DIR__ . '/../../../src/Core/Database/DBConnection.php';
-require_once __DIR__ . '/../../../vendor/autoload.php';
-require_once __DIR__ . '/../../../src/Domain/Services/SwapService.php';
-require_once __DIR__ . '/../../../src/Core/Config/LoadCountry.php';
+require_once __DIR__ . '/../../../../src/Core/Database/DBConnection.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../src/Domain/Services/SwapService.php';
+require_once __DIR__ . '/../../../../src/Core/Config/LoadCountry.php';
 
 use Core\Database\DBConnection;
 use Domain\Services\SwapService;
