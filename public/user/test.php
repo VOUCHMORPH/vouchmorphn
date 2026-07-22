15b0edd6c707aa564bb442fd0c3776326c3ad75e
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-22 14:42:23 +0200

    Update test.php

--- public/user/test.php
+++ public/user/test.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../../src/Infrastructure/Crypto/CertificateManager.php';
+require_once '../../src/Infrastructure/Crypto/CertificateManager.php';
 use Infrastructure\Crypto\CertificateManager;
 
 echo "========================================\n";
