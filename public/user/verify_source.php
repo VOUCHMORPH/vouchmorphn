d322c046249a4aee165cc9cdf8a3bc015da5f49e
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-23 03:30:11 +0200

    Update verify_source.php

--- public/user/verify_source.php
+++ public/user/verify_source.php
@@ -17,8 +17,8 @@
  * }
  */
 
-require_once __DIR__ . '/../../../bootstrap.php';
-require_once __DIR__ . '/../../../src/Services/SwapService.php';
+require_once __DIR__ . '/../../src/bootstrap.php';
+require_once __DIR__ . '/../../src/Domain/Services/SwapService.php';
 
 use VouchMorph\Services\SwapService;
 
