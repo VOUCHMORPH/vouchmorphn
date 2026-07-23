3323adb0faea0d65cbc981a473c9906b63c13638
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-23 03:48:34 +0200

    Update retry.php

--- public/api/v1/sources/retry.php
+++ public/api/v1/sources/retry.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../../../bootstrap.php';
+require_once __DIR__ . '/../../../../src/bootstrap.php';
 require_once __DIR__ . '/../../../../src/Application/Utils/SessionManager.php';
 require_once __DIR__ . '/../../../../src/Core/Database/DBConnection.php';
 require_once __DIR__ . '/../../../../src/Core/Config/LoadCountry.php';
