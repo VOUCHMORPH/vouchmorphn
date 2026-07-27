682c7f7a77e760c3ce8656de5238e54ae31d977e
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-27 22:13:29 +0200

    Update preview.php

--- public/admin/enterprise/imports/preview.php
+++ public/admin/enterprise/imports/preview.php
@@ -1,8 +1,8 @@
 <?php
-require_once '../auth.php';
+require_once __DIR__ . '/../../auth.php';
 $user = requireEnterpriseAuth();
-require_once '../../../../src/Core/Database/DBConnection.php';
-require_once '../../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../src/Core/Database/DBConnection.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use Core\Database\DBConnection;
 use PhpOffice\PhpSpreadsheet\IOFactory;
