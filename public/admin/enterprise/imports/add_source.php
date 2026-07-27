7fac2d1b89d40fb84a4e8e9db627db3fa36ec99d
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-27 20:30:12 +0200

    Update add_source.php

--- public/admin/enterprise/imports/add_source.php
+++ public/admin/enterprise/imports/add_source.php
@@ -1,8 +1,8 @@
 <?php
 // enterprise/imports/add_source.php - Manage source accounts (maker-checker controlled)
-require_once _DIR_ . '/../../auth.php';
+require_once __DIR__ . '/../../auth.php';
 $user = requireEnterpriseAuth();
-require_once _DIR_ . '/../../../../src/Core/Database/DBConnection.php';
+require_once __DIR__ . '/../../../../src/Core/Database/DBConnection.php';
 use Core\Database\DBConnection;
 
 $db = DBConnection::getConnection();
