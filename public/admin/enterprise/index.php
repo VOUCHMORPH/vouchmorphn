5927ca6f96926a044852be69da99bffb1d1e741d
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-27 22:03:04 +0200

    Update index.php

--- public/admin/enterprise/index.php
+++ public/admin/enterprise/index.php
@@ -16,8 +16,8 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once 'auth.php';
-require_once '../../../src/Domain/Services/DepartmentService.php';
+require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '/../../../src/Domain/Services/DepartmentService.php';
 use Domain\Services\DepartmentService;
 
 $user = requireEnterpriseAuth();
