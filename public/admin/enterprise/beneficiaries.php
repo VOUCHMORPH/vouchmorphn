1c689194ffdff202ca52cb7766ae207847f6b199
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-27 22:05:17 +0200

    Update beneficiaries.php

--- public/admin/enterprise/beneficiaries.php
+++ public/admin/enterprise/beneficiaries.php
@@ -3,7 +3,7 @@
  * beneficiaries.php - Beneficiary Management
  * Placeholder - Will be implemented later
  */
-require_once 'auth.php';
+require_once __DIR__ . '/../auth.php';
 $user = requireEnterpriseAuth();
 $orgName = htmlspecialchars($user['organization_name'] ?? 'Organization');
 
