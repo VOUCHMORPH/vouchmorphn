2ae3be515ac00b8aa168d93c32a29635bcf9aea8
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-28 07:45:31 +0200

    Update settings.php

--- public/admin/enterprise/settings.php
+++ public/admin/enterprise/settings.php
@@ -3,7 +3,7 @@
  * settings.php - Organization Settings
  * Placeholder - Will be implemented later
  */
-require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '/auth.php';
 $user = requireEnterpriseAuth();
 $orgName = htmlspecialchars($user['organization_name'] ?? 'Organization');
 
