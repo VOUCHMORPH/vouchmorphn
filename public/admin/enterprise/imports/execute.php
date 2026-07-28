316c791feb6e49f399edd00ced6037d2d99ed208
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-28 07:49:56 +0200

    Update execute.php

--- public/admin/enterprise/imports/execute.php
+++ public/admin/enterprise/imports/execute.php
@@ -13,7 +13,7 @@
  * ============================================================================
  */
 
-require_once __DIR__ . '/../../auth.php';
+require_once __DIR__ . '/../auth.php';
 $user = requireEnterpriseAuth();
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 require_once __DIR__ . '/../../../../src/Core/Database/DBConnection.php';
