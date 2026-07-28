e745145387bf94edc88e91f0f201c4b7dfd6fdcd
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-28 07:49:37 +0200

    Update approve.php

--- public/admin/enterprise/imports/approve.php
+++ public/admin/enterprise/imports/approve.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../auth.php';
+require_once __DIR__ . '/../auth.php';
 $user = requireEnterpriseAuth();
 require_once __DIR__ . '/../../../../src/Core/Database/DBConnection.php';
 use Core\Database\DBConnection;
