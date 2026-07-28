a8521ecd88ca0fb00da7aa863d9ac5e9e633fa02
marvinsehunelo <marvinsehunelo@gmail.com>
2026-07-28 07:43:35 +0200

    Update index.php

--- public/admin/enterprise/index.php
+++ public/admin/enterprise/index.php
@@ -16,7 +16,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once __DIR__ . '/../auth.php';
+require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/../../../src/Domain/Services/DepartmentService.php';
 use Domain\Services\DepartmentService;
 
