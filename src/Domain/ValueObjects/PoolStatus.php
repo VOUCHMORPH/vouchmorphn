45c38a9776d1704f71f154f6b39505259d1609b5
marvinsehunelo <marvinsehunelo@gmail.com>
2026-08-09 12:49:01 +0200

    Update PoolStatus.php

--- src/Domain/ValueObjects/PoolStatus.php
+++ src/Domain/ValueObjects/PoolStatus.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
-
 namespace Domain\ValueObjects;
-
 enum PoolStatus: string
 {
     case CREATED = 'CREATED';
@@ -11,6 +9,8 @@ enum PoolStatus: string
     case FUNDED = 'FUNDED';
     case DESTINATION_PENDING = 'DESTINATION_PENDING';
     case DESTINATION_COMPLETED = 'DESTINATION_COMPLETED';
+    case PENDING_CASHOUT = 'PENDING_CASHOUT';
+    case PENDING_IDENTITY_CLAIM = 'PENDING_IDENTITY_CLAIM';
     case DEBITING = 'DEBITING';
     case SETTLING = 'SETTLING';
     case INVOICING = 'INVOICING';
