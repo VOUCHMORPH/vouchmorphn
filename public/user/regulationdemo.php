5c7a0f584bfc2a52fefa9fd3c53e246a2cacf1d0
marvinsehunelo <marvinsehunelo@gmail.com>
2026-08-15 09:48:45 +0200

    Update regulationdemo.php

--- public/user/regulationdemo.php
+++ public/user/regulationdemo.php
@@ -15,7 +15,6 @@ if (!defined('APP_ROOT')) {
     define('APP_ROOT', rtrim(realpath(__DIR__ . '/../../'), '/') ?: '/var/www/html');
 }
 
-<?php
 // FIX 4: Load Composer autoloader with error suppression
 @include_once APP_ROOT . '/vendor/autoload.php';
 
