300abb736ebdfa72c3f47d74ea4418582d0aa94f
marvinsehunelo <marvinsehunelo@gmail.com>
2026-09-20 14:56:39 +0200

    Update file paths in release_expired_holds.php

--- src/cron/release_expired_holds.php
+++ src/cron/release_expired_holds.php
@@ -17,10 +17,10 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../src/Core/Database/DBConnection.php';
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../src/Domain/Services/SwapService.php';
-require_once __DIR__ . '/../src/Core/Config/LoadCountry.php';
+require_once __DIR__ . '/../../src/Core/Database/DBConnection.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../src/Domain/Services/SwapService.php';
+require_once __DIR__ . '/../../src/Core/Config/LoadCountry.php';
 
 use Core\Database\DBConnection;
 use Domain\Services\SwapService;
